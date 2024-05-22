8f0645e7f04f70663ac593f10f13f6e9d02b265b
M H Hasib <hasib.mahmudul@gmail.com>
2024-05-22 13:51:36 +0600

    feat: authentication done

---
+++ routes/api.php
@@ -0,0 +1,14 @@
+<?php
+
+use App\Http\Controllers\Api\AuthController;
+use Illuminate\Support\Facades\Route;
+
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/refresh', [AuthController::class, 'refreshToken']);
+
+Route::group(['middleware' => ['auth:api']], function () {
+    Route::get('/me', [AuthController::class, 'me']);
+    Route::post('/logout', [AuthController::class, 'logout']);
+
+});
